c20a54f4f447e57e30e82c4d73ec72a8c5b92bdf
Sawan jha <jhasawan02@gmail.com>
2026-03-31 14:54:11 +0530

    day5 - 2 easy questions done

--- day5/14.longest-common-prefix.cpp
+++ day5/14.longest-common-prefix.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
 /*
  * @lc app=leetcode id=14 lang=cpp
  *
@@ -8,7 +12,19 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
+        if(strs.empty()) return "";
+        string prefix = "";
+        for(int i = 0; i< strs[0].length(); i++){
+            char current = strs[0][i];
+
+            for(int j = 1; j <strs.size(); j++){
+                if(i >= strs[j].length() || strs[j][i] != current){
+                    return prefix;
+                }
+            }
+            prefix += current;
+        }
+        return prefix;
     }
 };
 // @lc code=end
